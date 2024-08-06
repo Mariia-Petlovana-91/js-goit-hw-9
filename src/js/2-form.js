d67e28120b3f8464d9b4eb6a75edf34d66eb9e2b
Mariia <90716920+Mariia-Petlovana-91@users.noreply.github.com>
2024-08-06 12:19:51 +0200

    without required

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,40 +1,48 @@
 const refs = {
-	form:document.querySelector('.form-js'),
-}
+	form: document.querySelector('.form-js'),
+};
+
 
 refs.form.addEventListener('input', collectFieldValues);
 refs.form.addEventListener('submit', sendForm);
 
-
 const STORAGE_KEY = "feedback-form-state";
-const formData = {};
+let formData = {};
 
-function getDataFromLocalStorage(){
-      const dataFromLocalStorage = JSON.parse( localStorage.getItem(STORAGE_KEY));
-      
-      if(dataFromLocalStorage){
-		for (let key in dataFromLocalStorage){
-			if (dataFromLocalStorage.hasOwnProperty(key)){
+
+function getDataFromLocalStorage() {
+	const dataFromLocalStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+	if (dataFromLocalStorage) {
+		formData = dataFromLocalStorage;
+		for (let key in dataFromLocalStorage) {
+			if (dataFromLocalStorage.hasOwnProperty(key)) {
 				refs.form.elements[key].value = dataFromLocalStorage[key];
 			}
 		}
 	}
-
-	
 }
 
 getDataFromLocalStorage();
 
-
-function collectFieldValues (e){
-	formData[e.target.name] = e.target.value;
-	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData).trim());	
+function collectFieldValues(e) {
+	formData[e.target.name] = e.target.value.trim();
+	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-
-function sendForm(e){
-	console.log(formData);
+function sendForm(e) {
 	e.preventDefault();
+
+	const allFieldsFilled = [...refs.form.elements].every(element => {
+		return element.name === "" || element.value.trim() !== "";
+	});
+
+	if (!allFieldsFilled) {
+		alert("Fill please all fields");
+		return;
+	}
+
+	console.log(formData);
 	e.currentTarget.reset();
 	localStorage.removeItem(STORAGE_KEY);
+	formData = {};
 }
